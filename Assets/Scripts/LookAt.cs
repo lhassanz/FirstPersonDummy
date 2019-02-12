1d132c7cb427d5c025cc78baffa0770eac8c47f8
lhassanz <laythh.zubaidi@gmail.com>
2019-02-12 12:00:41 -0800

    First Person Shooter Game of some sorts for CS485

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,21 @@
+﻿/* 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	public Transform offset;
+	
+
+	// Use this for initialization
+	void Start () {
+		offset = transfrom.position
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+*/
