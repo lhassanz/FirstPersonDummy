4230e7c140de3ab388f4e5d4fdc66d7bb4c081f5
lhassanz <laythh.zubaidi@gmail.com>
2019-02-12 14:40:39 -0800

    second commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	private float health;
+	private Vector3 offset;
+
+	private float maxHealth;
+	public GameObject explosion;
+	//private Text healthText;
+	//private Image healthBar;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1f;
+		offset = transform.position - target.position;
+
+		health = 100.0f;
+		maxHealth = 100.0f;
+		//healthText = transform.Find("Canvas").Find("Text").GetComponent<Text>();
+		//healthBar = transform.Find("Canvas").Find("Image").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target, Vector3.up);
+		transform.position += transform.forward * speed * Time.deltaTime;
+		//healthText.text = health.ToString();
+		//healthBar.fillAmount = health / maxHealth;
+	}
+
+	void OnCollisionEnter(Collision col){
+
+		if(col.gameObject.tag == "Bullet"){
+			health -= 10;
+			if(health < 1){
+				Destroy(this);
+				Instantiate(explosion, transform.position, transform.rotation);
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/FirstPerson.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour {
+
+
+	public float speed;
+	public float jumpHeight;
+	public LayerMask ground;
+	public Transform feet;
+
+	private Vector3 direction;
+	private Rigidbody rbody;
+
+	private float rotationSpeed = 1f;
+	private float rotationY = 10f;
+	private float rotationX = 0f;
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn; 
+
+	// Use this for initialization
+	void Start () {
+		speed = 4.0f;
+		jumpHeight = 3.0f;
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = Vector3.zero;
+		direction.x = Input.GetAxis("Horizontal");
+		direction.z = Input.GetAxis("Vertical");
+		direction = direction.normalized;
+		if(direction.x != 0) {
+			rbody.MovePosition(rbody.position + transform.right * direction.x * speed * Time.deltaTime);
+		}
+		if(direction.z != 0){
+			rbody.MovePosition(rbody.position + transform.forward * direction.z * speed * Time.deltaTime);
+		}
+
+		rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed;
+		rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+		bool isGrounded = Physics.CheckSphere(feet.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			rbody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+		}
+
+		if(Input.GetButtonDown("Fire1")){
+			Fire();
+		}
+	}
+
+	void Fire(){
+		var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+		Destroy(bullet, 4.0f);
+	}
+}
